97957c43a9e54ce06080d3cd97c23fe29dd6dd7d
Ahamed Abdullah <ahamed.abdullah00772@gmail.com>
2026-05-05 23:40:23 +0530

    added balanced bracket with wild card solution

--- src/BalancedBrackets.java
+++ src/BalancedBrackets.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 public class BalancedBrackets {
     public static boolean isBalancedBrackets(String input) {
         Stack<Character> characterStack = new Stack<>();
-        Map<Character,Character> bracketMap = Map.of(')', '(', '}', '{', ']', '[');
+        Map<Character, Character> bracketMap = Map.of(')', '(', '}', '{', ']', '[');
         for (char c : input.toCharArray()) {
             if (bracketMap.containsValue(c)) {
                 characterStack.push(c);
@@ -18,9 +18,34 @@ public class BalancedBrackets {
         return characterStack.isEmpty();
     }
 
+    public static boolean isBalancedWildCard(String input) {
+        int minOpen, maxOpen;
+        minOpen = maxOpen = 0;
+        for (Character c : input.toCharArray()) {
+            if (c.equals('(')) {
+                minOpen++;
+                maxOpen++;
+            } else if (c.equals(')')) {
+                minOpen--;
+                maxOpen--;
+            } else {
+                minOpen--;
+                maxOpen++;
+            }
+
+            if (minOpen < 0) minOpen = 0;
+            if (maxOpen < 0) return false;
+        }
+
+        return minOpen == 0;
+    }
+
     public static void main(String[] args) {
         System.out.println(isBalancedBrackets("{{}([])}"));
         System.out.println(isBalancedBrackets("{(})"));
+        System.out.println(isBalancedWildCard("(*)"));
+        System.out.println(isBalancedWildCard("*("));
+        System.out.println(isBalancedWildCard("(**"));
 
     }
 }
